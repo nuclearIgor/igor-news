9becba33c497d0bc50f7f716fe8fa953bdbb2efc
nuclearIgor <igorflribeiro@gmail.com>
2022-09-16 22:47:29 -0300

    feat: faunadb setup to save user email on sign in

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
+import {query as q} from 'faunadb'
+import {fauna} from '../../../services/fauna'
+
 export const authOptions = {
     providers: [
         GithubProvider({
@@ -9,6 +12,43 @@ export const authOptions = {
             // scope: 'read:user'
         }),
     ],
+    jwt: {
+        signingKey: process.env.SIGNING_KEY
+    },
+    callbacks: {
+        async signIn(user, account, profile){
+            const {email} = user.user
+
+            try {
+                await fauna.query(
+                   q.If(
+                       q.Not(
+                           q.Exists(
+                               q.Match(
+                                   q.Index('user_by_email'),
+                                   q.Casefold(email)
+                               )
+                           )
+                       ),
+                       q.Create(
+                           q.Collection('users'),
+                           {data: {email} }
+                       ),
+                   //    q.upate
+                       q.Get(
+                           q.Match(
+                                q.Index('user_by_email'),
+                                q.Casefold(email)
+                           )
+                       )
+                   )
+                )
+                return true
+            } catch {
+                return false
+            }
+        },
+    }
 }
 
 export default NextAuth(authOptions)
