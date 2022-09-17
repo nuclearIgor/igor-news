01af2c70a3445570d85889f7e007edac740f6815
nuclearIgor <igorflribeiro@gmail.com>
2022-09-16 23:35:34 -0300

    feat: stripe setup - create stripe customer profile redirect success or failure on payment

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,12 +1,37 @@
 import styles from './styles.module.scss'
+import {useSession, signIn} from "next-auth/react";
+import {api} from "../../services/api";
+import {getStripeJs} from "../../services/stripe-js";
 
 interface SubscribeButtonProps {
     priceId: string
 }
 
 export const SubscribeButton = ({priceId}: SubscribeButtonProps) => {
+    const {data: Session} = useSession()
+
+    const handleSubscribe = async () => {
+        if(!Session){
+            signIn('github')
+            return
+        }
+
+        try {
+            const response = await api.post('/subscribe')
+
+            const { sessionId } = response.data
+
+            const stripe = await getStripeJs()
+
+            await stripe.redirectToCheckout({sessionId: sessionId})
+
+        } catch (e){
+            alert(e.message)
+        }
+    }
+
     return (
-        <button className={styles.subscribeButton}>
+        <button className={styles.subscribeButton} onClick={() => handleSubscribe()}>
             Subscribe Now
         </button>
     );
